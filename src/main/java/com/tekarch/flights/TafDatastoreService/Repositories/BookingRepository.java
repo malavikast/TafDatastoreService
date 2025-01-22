320efb13d493863edbc9fd07a0e604a2132bd3c3
Malavika ST <malavikast@tekarch.com>
2025-01-21 20:51:28 -0800

    Updated the Query in Booking Repository

--- src/main/java/com/tekarch/flights/TafDatastoreService/Repositories/BookingRepository.java
+++ src/main/java/com/tekarch/flights/TafDatastoreService/Repositories/BookingRepository.java
@@ -2,12 +2,12 @@ package com.tekarch.flights.TafDatastoreService.Repositories;
 
 import com.tekarch.flights.TafDatastoreService.Models.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.NativeQuery;
 
 import java.util.List;
 
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 
-    @Query("select * from tafdb.bookings b where b.user_id = ?1")
+    @NativeQuery(value = "select * from bookings b where b.user_id = ?1")
     List<Booking> findBookingsByUserId(Long userId);
 }
